63f8e02dbdc70c7cd675466b7075458f5929fad1
Brodie Kurczynski <kurczynski@gmail.com>
2016-12-04 21:58:25 -0800

    Added ranges for input and changed to primitive ints.

--- java/src/main/java/org/kurczynski/interviewprogramming/lovelyluckylambs/Main.java
+++ java/src/main/java/org/kurczynski/interviewprogramming/lovelyluckylambs/Main.java
@@ -1,29 +1,35 @@
 package org.kurczynski.interviewprogramming.lovelyluckylambs;
 
-import java.math.BigInteger;
+import java.security.spec.InvalidParameterSpecException;
 
 public class Main {
+	public static final int MAX_LAMBS = 1000000000;
+	public static final int MIN_LAMBS = 10;
+
 	public static void main(String args[]) {
-		System.out.println(Main.answer(10));
-		System.out.println(Main.answer(143));
+		try {
+			System.out.println(Main.answer(10));
+			System.out.println(Main.answer(143));
+		} catch (InvalidParameterSpecException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static int generous(int lambs) {
-		Double tmp = ((Math.log(lambs) / Math.log(2)));
-		return tmp.intValue();
+		return (int) (Math.log(lambs) / Math.log(2));
 	}
 
 	public static int stingy(int lambs) {
-		BigInteger n1 = BigInteger.valueOf(1);
-		BigInteger n2 = BigInteger.valueOf(1);
-		BigInteger n;
+		int n1 = 1;
+		int n2 = 1;
+		int n;
 
-		BigInteger sum = n1.add(n2);
+		int sum = n1 + n2;
 		int counter = 1;
 
-		while (sum.compareTo(BigInteger.valueOf(lambs)) <= 0) {
-			n = n2.add(n1);
-			sum = sum.add(n);
+		while (sum < lambs) {
+			n = n2 + n1;
+			sum = sum + n;
 			n2 = n1;
 			n1 = n;
 
@@ -33,7 +39,11 @@ public class Main {
 		return counter;
 	}
 
-	public static int answer(int lambs) {
+	public static int answer(int lambs) throws InvalidParameterSpecException {
+		if (lambs < Main.MIN_LAMBS || lambs > Main.MAX_LAMBS)
+			throw new InvalidParameterSpecException(
+					"LAMBs must be between " + Main.MIN_LAMBS + " and " + Main.MAX_LAMBS);
+
 		return Main.stingy(lambs) - Main.generous(lambs);
 	}
 }
