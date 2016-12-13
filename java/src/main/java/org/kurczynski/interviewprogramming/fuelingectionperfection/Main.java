dfe97ba996faa3be297d5f300b8b2dbcf61a1ec0
Brodie Kurczynski <kurczynski@gmail.com>
2016-12-13 09:44:51 -0800

    Updated pom to use Java 7.
    Initial attempt at the solution.

--- java/src/main/java/org/kurczynski/interviewprogramming/fuelingectionperfection/Main.java
+++ java/src/main/java/org/kurczynski/interviewprogramming/fuelingectionperfection/Main.java
@@ -1,4 +1,33 @@
 package org.kurczynski.interviewprogramming.fuelingectionperfection;
 
 public class Main {
+	public static void main(String args[]) {
+		System.out.println(Main.answer("15"));
+		System.out.println(Main.answer("4"));
+	}
+
+	static int answer(String fuelPellets) {
+		int counter = 0;
+		int fuelPelletsInt = Integer.parseInt(fuelPellets);
+		Double output = Math.log(Integer.parseInt(fuelPellets)) / Math.log(2);
+		int offset = Math.round(output - Math.floor(output)) == 0 ? -1 : 1;
+		long base = Math.round(output);
+
+		Double roundedTwo = Math.pow(2, base);
+
+		/* Get the pallets to a power of two. */
+		while (fuelPelletsInt != roundedTwo.intValue()) {
+			fuelPelletsInt += offset;
+
+			counter++;
+		}
+
+		while (fuelPelletsInt != 1) {
+			fuelPelletsInt = fuelPelletsInt / 2;
+
+			counter++;
+		}
+
+		return counter;
+	}
 }
